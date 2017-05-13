522b5af760fafb3d53a4ffbbea4fc538b19f1823
Cynthia <cynthia.tedesco@gmail.com>
2017-05-13 15:58:35 -0300

    test and format

--- src/main/java/model/Edge.java
+++ src/main/java/model/Edge.java
@@ -1,36 +1,37 @@
 package main.java.model;
 
-public class Edge  {
-    private final String id;
-    private final Vertex source;
-    private final Vertex destination;
-    private final int weight;
+public class Edge {
+	private final String id;
+	private final Vertex source;
+	private final Vertex destination;
+	private final int weight;
 
-    public Edge(String id, Vertex source, Vertex destination, int weight) {
-        this.id = id;
-        this.source = source;
-        this.destination = destination;
-        this.weight = weight;
-    }
+	public Edge(String id, Vertex source, Vertex destination, int weight) {
+		this.id = id;
+		this.source = source;
+		this.destination = destination;
+		this.weight = weight;
+	}
 
-    public String getId() {
-        return id;
-    }
-    public Vertex getDestination() {
-        return destination;
-    }
+	public String getId() {
+		return id;
+	}
 
-    public Vertex getSource() {
-        return source;
-    }
-    public int getWeight() {
-        return weight;
-    }
+	public Vertex getDestination() {
+		return destination;
+	}
 
-    @Override
-    public String toString() {
-        return source + " " + destination;
-    }
+	public Vertex getSource() {
+		return source;
+	}
 
+	public int getWeight() {
+		return weight;
+	}
+
+	@Override
+	public String toString() {
+		return source + " " + destination;
+	}
 
 }
